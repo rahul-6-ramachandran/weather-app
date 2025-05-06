70c44bab99e432c408c5df99c11bc0f45cfb4919
rahul_r_432 <rahulramachandran432@gmail.com>
2025-05-06 12:05:38 +0530

    feat : initial server and mongodb setup

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import { configDotenv } from 'dotenv'
+import mongoose from 'mongoose';
+
+configDotenv()
+
+const app = express()
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB connected successfully"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+const port = process.env.PORT || 2020
+
+
+
+app.get('/', (req, res) => {
+	res.send("Helloooo");
+});
+
+
+app.listen(port, ()=>{
+    console.log("Server Running Succesfully")
+})
+
